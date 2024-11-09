d38ecc6f3add32c81c19a33b879341c998e7d6a5
Merwinmiles <143722357+Merwinmiles@users.noreply.github.com>
2024-11-08 23:17:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function togglebox() {
+    if(document.getElementById("left").style.display === "block") {
+        document.getElementById("left").style.display = "none";
+    }
+    else {
+        document.getElementById("left").style.display = "block";
+    }
+}
