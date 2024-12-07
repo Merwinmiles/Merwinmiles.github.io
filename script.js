3eaf577d7f337680d7f955febc9ea3c1daf5e9c3
Merwinmiles <143722357+Merwinmiles@users.noreply.github.com>
2024-12-06 21:51:20 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function togglebox() {
-    if(document.getElementById("left").style.display === "block") {
-        document.getElementById("left").style.display = "none";
-    }
-    else {
-        document.getElementById("left").style.display = "block";
-    }
-}
